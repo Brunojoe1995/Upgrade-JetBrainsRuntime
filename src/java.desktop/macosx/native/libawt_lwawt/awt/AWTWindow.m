JetBrains/JetBrainsRuntime

JBR-1874 Cursor not changing from 'default' to 'text'. Additional fix after reopening.
(cherry picked from commit 5a29d4ade9987acaf3d4b28772bc8f2f5f02ae7a)

src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -109,7 +109,12 @@ static NSPoint lastTopLeftPoint;
 - (void)sendEvent:(NSEvent *)event {                            \
     [(AWTWindow*)[self delegate] sendEvent:event];              \
     [super sendEvent:event];                                    \
-}
+}                                                               \
+                                                                \
+- (void)becomeKeyWindow {                                       \
+    [super becomeKeyWindow];                                    \
+    [(AWTWindow*)[self delegate] becomeKeyWindow];              \
+}                                                               \
 
 @implementation AWTWindow_Normal
 AWT_NS_WINDOW_IMPLEMENTATION
@@ -546,6 +551,17 @@ AWT_ASSERT_APPKIT_THREAD;
 - (BOOL) canBecomeKeyWindow {
 AWT_ASSERT_APPKIT_THREAD;
     return self.isEnabled && (IS(self.styleBits, SHOULD_BECOME_KEY) || [self isSimpleWindowOwnedByEmbeddedFrame]);
+}
+
+- (void) becomeKeyWindow {
+    AWT_ASSERT_APPKIT_THREAD;
+
+    // Reset current cursor in CCursorManager such that any following mouse update event
+    // restores the correct cursor to the frame context specific one.
+    JNIEnv *env = [ThreadUtilities getJNIEnv];
+    DECLARE_CLASS(jc_CCursorManager, "sun/lwawt/macosx/CCursorManager");
+    DECLARE_METHOD(sjm_resetCurrentCursor, jc_CCursorManager, "resetCurrentCursor", "()V");
+    (*env)->CallStaticVoidMethod(env, jc_CCursorManager, sjm_resetCurrentCursor);
 }
 
 - (BOOL) canBecomeMainWindow {

Brunojoe1995/Upgrade-JetBrainsRuntime

(cherry picked from commit da44860f22dabf55b475c0fb9511f006aeb0883c)

src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -109,7 +109,12 @@ static NSPoint lastTopLeftPoint;
 - (void)sendEvent:(NSEvent *)event {                            \
     [(AWTWindow*)[self delegate] sendEvent:event];              \
     [super sendEvent:event];                                    \
-}
+}                                                               \
+                                                                \
+- (void)becomeKeyWindow {                                       \
+    [super becomeKeyWindow];                                    \
+    [(AWTWindow*)[self delegate] becomeKeyWindow];              \
+}                                                               \
 
 @implementation AWTWindow_Normal
 AWT_NS_WINDOW_IMPLEMENTATION
@@ -546,6 +551,17 @@ AWT_ASSERT_APPKIT_THREAD;
 - (BOOL) canBecomeKeyWindow {
 AWT_ASSERT_APPKIT_THREAD;
     return self.isEnabled && (IS(self.styleBits, SHOULD_BECOME_KEY) || [self isSimpleWindowOwnedByEmbeddedFrame]);
+}
+
+- (void) becomeKeyWindow {
+    AWT_ASSERT_APPKIT_THREAD;
+
+    // Reset current cursor in CCursorManager such that any following mouse update event
+    // restores the correct cursor to the frame context specific one.
+    JNIEnv *env = [ThreadUtilities getJNIEnv];
+    DECLARE_CLASS(jc_CCursorManager, "sun/lwawt/macosx/CCursorManager");
+    DECLARE_STATIC_METHOD(sjm_resetCurrentCursor, jc_CCursorManager, "resetCurrentCursor", "()V");
+    (*env)->CallStaticVoidMethod(env, jc_CCursorManager, sjm_resetCurrentCursor);
 }
 
 - (BOOL) canBecomeMainWindow {