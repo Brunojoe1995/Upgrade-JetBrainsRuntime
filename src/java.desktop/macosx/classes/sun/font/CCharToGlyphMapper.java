yefengchun/JetBrainsRuntime

JRE-469 Console with emoji output becomes slow
The fix consists of two parts:
* Making CCharToGlyphMapper remember that a particular character
  cannot be displayed (isn't mapped to glyph with given font). Checking
  this repeatedly in native code is very slow.
* Make CCompositeGlyphMapper remember the results of char-to-glyph
  mapping, this was missing in previous implementation. This reuses
  caching code in CompositeGlyphMapper, extending the range of
  characters for which the results are cached to include Supplementary
  Multilingual Plane (most emoji characters belong to it).

port commit 4e0ccde2 from JBR 9

src/java.desktop/macosx/classes/sun/font/CCharToGlyphMapper.java
@@ -28,6 +28,8 @@ package sun.font;
 import java.util.HashMap;
 
 public class CCharToGlyphMapper extends CharToGlyphMapper {
+    private static final int UNMAPPED_CHAR = Integer.MIN_VALUE;
+
     private static native int countGlyphs(final long nativeFontPtr);
 
     private Cache cache = new Cache();
@@ -90,15 +92,16 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
 
     public synchronized int charToGlyph(char unicode) {
         final int glyph = cache.get(unicode);
-        if (glyph != 0) return glyph;
+        if (glyph != 0) return glyph == UNMAPPED_CHAR ? 0 : glyph;
 
         final char[] unicodeArray = new char[] { unicode };
         final int[] glyphArray = new int[1];
 
         nativeCharsToGlyphs(fFont.getNativeFontPtr(), 1, unicodeArray, glyphArray);
-        cache.put(unicode, glyphArray[0]);
+        int result = glyphArray[0];
+        cache.put(unicode, result == 0 ? UNMAPPED_CHAR : result);
 
-        return glyphArray[0];
+        return result;
     }
 
     public synchronized int charToGlyph(int unicode) {
@@ -243,7 +246,7 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
 
                 final int value = get(code);
                 if (value != 0 && value != -1) {
-                    values[i] = value;
+                    values[i] = value == UNMAPPED_CHAR ? 0 : value;
                     if (code >= 0x10000) {
                         values[i+1] = INVISIBLE_GLYPH_ID;
                         i++;
@@ -288,9 +291,10 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
                             low - LO_SURROGATE_START + 0x10000;
                     }
                 }
-               values[i] = glyphCodes[m];
-               put(code, values[i]);
-               if (code >= 0x10000) {
+                values[i] = glyphCodes[m];
+                int glyphCode = values[i];
+                put(code, glyphCode == 0 ? UNMAPPED_CHAR : glyphCode);
+                if (code >= 0x10000) {
                    m++;
                    values[i + 1] = INVISIBLE_GLYPH_ID;
                 }

Brunojoe1995/Upgrade-JetBrainsRuntime

* Making CCharToGlyphMapper remember that a particular character cannot be displayed (isn't mapped to glyph with given font). Checking this repeatedly in native code is very slow.
* Make CCompositeGlyphMapper remember the results of char-to-glyph mapping, this was missing in previous implementation. This reuses caching code in CompositeGlyphMapper, extending the range of characters for which the results are cached to include Supplementary Multilingual Plane (most emoji characters belong to it).
port from JBR 11 to JBR 15 (cherry picked from commit 394e055ae673a6f8b6090eb3ae748f33b176cec7)
cherry picked from commit 0db7e948af8e1f210ad2abd4e1876920cf204f3b
(cherry picked from commit 7721393e5b8734925c484d9730668b186181fcb2)

src/java.desktop/macosx/classes/sun/font/CCharToGlyphMapper.java
@@ -28,6 +28,8 @@ package sun.font;
 import java.util.HashMap;
 
 public class CCharToGlyphMapper extends CharToGlyphMapper {
+    private static final int UNMAPPED_CHAR = Integer.MIN_VALUE;
+
     private static native int countGlyphs(final long nativeFontPtr);
 
     private Cache cache = new Cache();
@@ -90,7 +92,7 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
 
     public synchronized int charToGlyph(char unicode) {
         int glyph = cache.get(unicode);
-        if (glyph != 0) return glyph;
+        if (glyph != 0) return glyph == UNMAPPED_CHAR ? 0 : glyph;
 
         if (FontUtilities.isDefaultIgnorable(unicode)) {
             glyph = INVISIBLE_GLYPH_ID;
@@ -101,7 +103,7 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
             glyph = glyphArray[0];
         }
 
-        cache.put(unicode, glyph);
+        cache.put(unicode, glyph == 0 ? UNMAPPED_CHAR : glyph);
 
         return glyph;
     }
@@ -248,7 +250,7 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
 
                 final int value = get(code);
                 if (value != 0 && value != -1) {
-                    values[i] = value;
+                    values[i] = value == UNMAPPED_CHAR ? 0 : value;
                     if (code >= 0x10000) {
                         values[i+1] = INVISIBLE_GLYPH_ID;
                         i++;
@@ -296,9 +298,10 @@ public class CCharToGlyphMapper extends CharToGlyphMapper {
                             low - LO_SURROGATE_START + 0x10000;
                     }
                 }
-               values[i] = glyphCodes[m];
-               put(code, values[i]);
-               if (code >= 0x10000) {
+                values[i] = glyphCodes[m];
+                int glyphCode = values[i];
+                put(code, glyphCode == 0 ? UNMAPPED_CHAR : glyphCode);
+                if (code >= 0x10000) {
                    m++;
                    values[i + 1] = INVISIBLE_GLYPH_ID;
                 }