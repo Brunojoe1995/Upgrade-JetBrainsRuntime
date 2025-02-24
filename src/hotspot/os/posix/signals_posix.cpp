JetBrains/JetBrainsRuntime

JBR-4562 JBR-4610 Generate hs_err file on SIGABRT
Supported on Linux and MacOS only.
Controlled by -Djbr.catch.SIGABRT=true option; off by default.

src/hotspot/os/posix/signals_posix.cpp
@@ -564,6 +564,12 @@ int JVM_HANDLE_XXX_SIGNAL(int sig, siginfo_t* info,
     return true; // ignore it
   }
 
+  if (sig == SIGABRT) {
+      // Re-set the handler so that we don't recurse if/when abort() is called
+      // from here.
+      os::signal(SIGABRT, (void*)SIG_DFL);
+  }
+
   // Note: it's not uncommon that JNI code uses signal/sigset to install,
   // then restore certain signal handler (e.g. to temporarily block SIGPIPE,
   // or have a SIGILL handler when detecting CPU type). When that happens,
@@ -1298,6 +1304,9 @@ void install_signal_handlers() {
     // that an attach client accidentally forces HotSpot to quit prematurely. We skip the periodic
     // check because late initialization will overwrite it to UserHandler.
     set_signal_handler(BREAK_SIGNAL, false);
+  }
+  if (CatchSIGABRT) {
+    set_signal_handler(SIGABRT);
   }
 #if defined(__APPLE__)
   // lldb (gdb) installs both standard BSD signal handlers, and mach exception

Brunojoe1995/Upgrade-JetBrainsRuntime

with fix for JBR-5300 Change source code and test files to use GPL license
(cherry picked from commit 987ce225b9812563bb55e2bdea777265928fbfed)

src/hotspot/os/posix/signals_posix.cpp
@@ -551,6 +551,12 @@ int JVM_HANDLE_XXX_SIGNAL(int sig, siginfo_t* info,
 {
   assert(info != nullptr && ucVoid != nullptr, "sanity");
 
+  if (sig == SIGABRT) {
+      // Re-set the handler so that we don't recurse if/when abort() is called
+      // from here.
+      PosixSignals::install_generic_signal_handler(SIGABRT, (void*)SIG_DFL);
+  }
+
   // Note: it's not uncommon that JNI code uses signal/sigset to install,
   // then restore certain signal handler (e.g. to temporarily block SIGPIPE,
   // or have a SIGILL handler when detecting CPU type). When that happens,
@@ -1330,6 +1336,10 @@ static void install_signal_handlers() {
     int ret =  PosixSignals::install_sigaction_signal_handler(&sigAct, &oldSigAct,
                                                               BREAK_SIGNAL, UserHandler);
     assert(ret == 0, "check");
+  }
+
+  if (CatchSIGABRT) {
+    set_signal_handler(SIGABRT);
   }
 
 #if defined(__APPLE__)