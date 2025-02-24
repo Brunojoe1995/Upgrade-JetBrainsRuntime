JetBrains/JetBrainsRuntime

JBR-2630 Typing speed in IDE editor was dropped after switching to 11.0.8
New version of the fix as 0c16f30a caused java.security.AccessControlException on pre-commit.
Priviledged action is now ised to obtain sun.awt.osx.RobotSafeDelayMillis property value.

src/java.desktop/macosx/classes/sun/lwawt/macosx/CRobot.java
@@ -31,10 +31,12 @@ import java.awt.Robot;
 import java.awt.peer.RobotPeer;
 
 import sun.awt.CGraphicsDevice;
+import sun.security.action.GetIntegerAction;
 
 final class CRobot implements RobotPeer {
 
     private static final int MOUSE_LOCATION_UNKNOWN      = -1;
+    private static final int DEFAULT_SAFE_DELAY_MILLIS   = 50;
 
     private final CGraphicsDevice fDevice;
     private int mouseLastX = MOUSE_LOCATION_UNKNOWN;
@@ -51,7 +53,12 @@ final class CRobot implements RobotPeer {
      */
     public CRobot(Robot r, CGraphicsDevice d) {
         fDevice = d;
-        initRobot();
+        int safeDelayMillis = GetIntegerAction.privilegedGetProperty(
+                "sun.awt.osx.RobotSafeDelayMillis", DEFAULT_SAFE_DELAY_MILLIS);
+        if (safeDelayMillis < 0) {
+            safeDelayMillis = DEFAULT_SAFE_DELAY_MILLIS;
+        }
+        initRobot(safeDelayMillis);
     }
 
     @Override
@@ -193,7 +200,7 @@ final class CRobot implements RobotPeer {
         return c;
     }
 
-    private native void initRobot();
+    private native void initRobot(int safeDelayMillis);
     private native void mouseEvent(int lastX, int lastY, int buttonsState,
                                    boolean isButtonsDownState,
                                    boolean isMouseMove);

Brunojoe1995/Upgrade-JetBrainsRuntime

Introduced sun.awt.osx.RobotSafeDelayMillis property to control macOS specific safe delay for Robot methods.
50 ms safe delay was initially hardcoded in 3862142d (JDK-8242174: [macos] The NestedModelessDialogTest test make the macOS unstable) which affected performance tests execution.
(cherry picked from commit 5f691bb78842b6cd1cd46259daacf640b8adcd27)
(cherry picked from commit cfc90ce2c5a7351dcd85cb8e13ce393148384183)

src/java.desktop/macosx/classes/sun/lwawt/macosx/CRobot.java
@@ -34,6 +34,7 @@ import sun.awt.CGraphicsDevice;
 final class CRobot implements RobotPeer {
 
     private static final int MOUSE_LOCATION_UNKNOWN      = -1;
+    private static final int DEFAULT_SAFE_DELAY_MILLIS   = 50;
 
     private final CGraphicsDevice fDevice;
     private int mouseLastX = MOUSE_LOCATION_UNKNOWN;
@@ -50,7 +51,12 @@ final class CRobot implements RobotPeer {
      */
     CRobot(CGraphicsDevice d) {
         fDevice = d;
-        initRobot();
+        int safeDelayMillis = Integer.getInteger(
+                "sun.awt.osx.RobotSafeDelayMillis", DEFAULT_SAFE_DELAY_MILLIS);
+        if (safeDelayMillis < 0) {
+            safeDelayMillis = DEFAULT_SAFE_DELAY_MILLIS;
+        }
+        initRobot(safeDelayMillis);
     }
 
     /**
@@ -187,7 +193,7 @@ final class CRobot implements RobotPeer {
         return c;
     }
 
-    private native void initRobot();
+    private native void initRobot(int safeDelayMillis);
     private native void mouseEvent(int lastX, int lastY, int buttonsState,
                                    boolean isButtonsDownState,
                                    boolean isMouseMove);